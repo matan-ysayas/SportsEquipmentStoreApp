1732f371c8e0355b63f0f06e83cb24a4545b4fb9
matan-ysayas <matan.ysayas@gmail.com>
2022-01-22 13:16:24 +0200

    Add project files.

---
+++ SportsEquipmentStoreApp/Controllers/ShoesController.cs
@@ -0,0 +1,27 @@
+﻿using SportsEquipmentStoreApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsEquipmentStoreApp.Controllers
+{
+    public class ShoesController : Controller
+
+    {
+      public static  string ConnecsionString = "Data Source=laptop-e49vkatt;Initial Catalog = SchoolAdoDB; Integrated Security = True; Pooling=False";
+               SportStoreDBDataContext SportStoreDB = new SportStoreDBDataContext(ConnecsionString);
+        // GET: Shoes
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        public ActionResult AllShoes()
+        {
+            SportStoreDB.Shoes.ToList();
+            return View(SportStoreDB.Shoes.ToList());
+        }
+    }
+}
---
+++ SportsEquipmentStoreApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SportsEquipmentStoreApp.MvcApplication" Language="C#" %>
