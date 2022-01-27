83b77b6ca503696dd80581af56717ba01b3ec2ac
matan-ysayas <matan.ysayas@gmail.com>
2022-01-27 12:42:36 +0200

    pants controller

---
+++ SportsEquipmentStoreApp/Controllers/PantsController.cs
@@ -0,0 +1,45 @@
+﻿using SportsEquipmentStoreApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsEquipmentStoreApp.Controllers
+{
+    public class PantsController : Controller
+    {
+        public static string ConnecsionString = "Data Source=.;Initial Catalog=SportStoreDB;Integrated Security=True;Pooling=False";
+        SportStoreDBDataContext SportStoreDB = new SportStoreDBDataContext(ConnecsionString);
+        // GET: Pants
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AllPants()
+        {
+            return View(SportStoreDB.Clothings.Where((item) => item.TypeOfClothing == "pants").ToList());
+        }
+        public ActionResult PantsManagerTable()
+        {
+            return View(SportStoreDB.Clothings.Where((item) => item.TypeOfClothing == "pants").ToList());
+        }
+        public ActionResult ShortPants()
+        {
+            return View(SportStoreDB.Clothings.Where((item) => item.TypeOfClothing == "pants" && item.IsItShort == true).ToList());
+        }
+        public ActionResult LongPants()
+        {
+            return View(SportStoreDB.Clothings.Where((item) => item.TypeOfClothing == "pants" && item.IsItShort == false).ToList());
+        }
+        public ActionResult DreyfitOnly()
+        {
+            return View(SportStoreDB.Clothings.Where((item) => item.TypeOfClothing == "pants" && item.IsItDreyfit == true).ToList());
+        }
+        public ActionResult SortedByPrice()
+        {
+            return View(SportStoreDB.Clothings.Where((item) => item.TypeOfClothing == "pants").OrderBy((item) => item.Price).ToList());
+        }
+    }
+}
